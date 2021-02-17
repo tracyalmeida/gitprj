a698e30910cdc621eecb9bb0aeb5b407bde8fc96
Tracy <Tracy@LAPTOP-4K3I9VII>
2021-02-17 12:07:40 +0530

    constructor and hello world

--- src/gitprj/student.java
+++ src/gitprj/student.java
@@ -1,10 +1,14 @@
 package gitprj;
 
 public class student {
+	student(){
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("hello");
+     System.out.println("hello world");
 	}
 
 }
