9b3af863cffc8d41537991d9ba0216f69322d77a
Tracy <Tracy@LAPTOP-4K3I9VII>
2021-02-17 13:40:59 +0530

    display added

--- src/gitprj/student.java
+++ src/gitprj/student.java
@@ -4,7 +4,10 @@ public class student {
 	student(){
 		
 	}
-
+	void display()
+	{
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      System.out.println("hello");
